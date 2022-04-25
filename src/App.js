68da6b2cfd244adc8cf7fe9096ea10c4e23c0243
luqmansatay <mhisham@guidewire.com>
2022-04-25 15:01:42 +0800

    Excel Upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { ExcelUploader } from './views/ExcelUploader';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Preview } from './views/ExcelPreview';
+import {BrowserRouter, Route, Link, Routes} from 'react-router-dom';
+
+import Home from './views/Login.js';
 import './App.css';
 
 function App() {
+    const [files, setFiles] = useState([]);
+    const onSuccess = (savedFiles) => {
+        setFiles(savedFiles)
+    };
+
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Link to="/">Home</Link><br></br>
+      <Link to="/Excel">Excel</Link>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/Excel" element={<ExcelUploader onSuccess={onSuccess}/>} />
+          
+          <Route path="/preview" element={<Preview files={files}/>}/>
+          
+          
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/views/ExcelUploader/index.js
@@ -0,0 +1,117 @@
+import {useState} from 'react';
+import axios from 'axios';
+import { toast} from 'react-toastify';
+import {ProgressBar} from 'react-bootstrap';
+import { IsEqual } from 'react-lodash'
+
+
+import './style.css';
+var t;
+
+export const ExcelUploader = ({onSuccess}) => {
+    const [files, setFiles] = useState([]);
+    
+    var bim = "BIM.xlsx";
+    
+    const onInputChange = (e) => {
+        setFiles(e.target.files)
+        
+        console.log(typeof e.target.files[0].name)
+        
+        if((e.target.files[0].name == "BIM.xlsx")||(e.target.files[0].name == "BIM.xls")){
+            t = "BIM.xlsx"
+            
+        }
+        
+        
+        
+    };
+
+    
+   
+
+    
+        
+        
+    
+
+    
+
+    const onSubmit = (e) => {
+        
+        e.preventDefault();
+
+        
+        
+        if(t === bim){
+
+        const data = new FormData();
+
+        for(let i = 0; i < files.length; i++) {
+            
+            data.append('file', files[i]);
+            
+            
+            
+        }
+        
+        
+
+        axios.post('//localhost:8000/upload', data,{headers:
+        {
+            'Content-Disposition': "attachment;",
+            'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        },responseType: 'arraybuffer',
+            onUploadProgress: progressEvent => {
+              const percentCompleted = Math.round(
+                (progressEvent.loaded * 100) / progressEvent.total
+              );
+              console.log(`upload process: ${percentCompleted}%`);
+              
+            }
+            
+          })
+            .then((response) => {
+                toast.success('Upload Success');
+                window.alert("Upload BIM.xlsx successfully")
+                
+                onSuccess(response.data)
+            })
+            .catch((e) => {
+                toast.error('Upload Error')
+                
+                window.alert("Cannot connect to server.\nPlease make sure you are connected to the Internet and try again");
+                
+            })
+        }
+        else{
+            window.alert("Unsuccessful upload BIM.xlsx\n-Please Select the Correct File (BIM.xlsx only)")
+            e.preventDefault();
+        }
+    };
+
+    
+    
+    return (
+        <form method="post" action="#" id="#" onSubmit={onSubmit}>
+            <div className="form-group files">
+                <h1>Upload Your BIM.xlsx File </h1>
+                <input directory="BIM.xlsx" type="file"
+                id="BIM"
+                name="BIM"
+                
+                      
+                       accept='.xlsx'
+                       onChange={onInputChange}
+                       className="form-control"
+                       />
+                       <ul id="listing"></ul>
+            </div>
+            
+            
+            <h3>Only .xlsx file will be accepted</h3>
+            <button>Submit</button>
+            
+        </form>
+    )
+};
