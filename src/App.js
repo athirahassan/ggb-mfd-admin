afce657379e31fc7d33ea3d8ef340f06d00db587
athirahassan <shassan@guidewire.com>
2022-04-26 09:37:46 +0800

    DKIP-151: excel Word duplicate for ExcelUploader

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import { useState } from 'react';
-import { ExcelUploader } from './views/ExcelUploader';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Preview } from './views/ExcelPreview';
 import {BrowserRouter, Route, Link, Routes} from 'react-router-dom';
 
-import Home from './views/Login.js';
+// views component
+import { ExcelUploader } from './views/ExcelUploader';
+import { Preview } from './views/ExcelPreview';
+
+import Login from './views/Login.js';
 import './App.css';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
     const [files, setFiles] = useState([]);
     const onSuccess = (savedFiles) => {
         setFiles(savedFiles)
     };
-
     
-
   return (
     <div className="App">
       <BrowserRouter>
-      <Link to="/">Home</Link><br></br>
+      <Link to="/">Login</Link><br></br>
       <Link to="/Excel">Excel</Link>
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route path="/Excel" element={<ExcelUploader onSuccess={onSuccess}/>} />
-          
+        <Route exact path="/" element={<Login/>}/>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/Excel" element={<ExcelUploader onSuccess={onSuccess}/>} />
           <Route path="/preview" element={<Preview files={files}/>}/>
-          
-          
+        </Route>       
       </Routes>
       </BrowserRouter>
-      
     </div>
   );
 }
