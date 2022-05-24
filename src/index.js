764737f84b6a7d6f519fa794e6cc3f10a9315511
athirahassan <102649524+athirahassan@users.noreply.github.com>
2022-05-24 11:59:33 +0800

    User/athirahassan/dkip 150 to merge luqman (#9)
    * app.css full bg
    
    * User/athirahassan/DKIP-147-bugfix (#6)
    
    * revamp directory (will not redirect to App.js)
    
    * bug fix: window now can load Dashboard
    
    * bug fix: removed import Button
    
    * added new validation function BIM & Group sheets
    
    * changes in index.js
    
    Co-authored-by: nmdrizal <nmdrizal@guidewire.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Dashboard from './Dashboard';
+import Login from './views/Login';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    {console.log("[Index] sessionStorage: "+sessionStorage.getItem("email"))}
-    <App />
+    {console.log("[Index] "+sessionStorage.getItem("email"))}
+    
+    {sessionStorage.getItem('email') === 'null' || sessionStorage.getItem('email') === null ? <Login /> : <Dashboard />}
+    
+    {/* <App /> */}
+    {/* <Login /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
